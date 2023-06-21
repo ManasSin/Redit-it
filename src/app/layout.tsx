dbcde70edcf20e6e0a293f426f74b0c9f22be3aa
ManasSin <maltisadan12@gmail.com>
2023-06-22 00:59:08 +0530

    setting up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
-import '@/styles/globals.css'
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Readit-it",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en" className={cn()}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
